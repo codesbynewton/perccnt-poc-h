1b53bf98db2e577bdbb560387934746b55436a08
Hari Chandra Prasad Giddaluru <harishchandranewton814@gmail.com>
2025-03-16 23:28:38 +0530

    Init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{fontFamily:"georgia", fontWeight:"bold", fontSize:20}}>Hello world </Text>
+    </View>
+  );
+}
